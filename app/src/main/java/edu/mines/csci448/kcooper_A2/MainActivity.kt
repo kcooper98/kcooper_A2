c480cc7106627f341ea82ec679f28dac716a8e39
Kyle Cooper <kcooper@mines.edu>
2020-03-10 22:21:55 -0600

    Did stuff and things

--- app/src/main/java/edu/mines/csci448/kcooper_A2/MainActivity.kt
+++ app/src/main/java/edu/mines/csci448/kcooper_A2/MainActivity.kt
@@ -2,11 +2,18 @@ package edu.mines.csci448.kcooper_A2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.mines.csci448.kcooper_A2.ui.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = WelcomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
     }
 }
--- app/src/main/java/edu/mines/csci448/kcooper_A2/ui/WelcomeFragment.kt
+++ app/src/main/java/edu/mines/csci448/kcooper_A2/ui/WelcomeFragment.kt
@@ -1,7 +1,41 @@
 package edu.mines.csci448.kcooper_A2.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
+import edu.mines.csci448.kcooper_A2.R
 
 class WelcomeFragment: Fragment() {
+    private val logTag = "448.WelcomeFrag"
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(logTag, "oncCreate() called")
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(logTag, "onOptionsItemSelected() called")
+        return when (item.itemId) {
+            R.id. // TODO: connect options menu to the game fragment, implement callbacks and link to main activity
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(logTag, "onCreateView() called")
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        Log.d(logTag, "onCreateOptionsMenu() called")
+        inflater.inflate(R.menu.fragment_welcome, menu)
+    }
 }
